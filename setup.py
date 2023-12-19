2b814f92ff8f0f3fbb3f99fd56c64b0103c17381
NewGuy103 <122655683+NewGuy103@users.noreply.github.com>
2023-12-19 18:12:59 +0800

    Create setup.py
    Created setup.py since it's required to publish to PyPi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newguy103-pycrypter',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['cryptography'],
+    author='NewGuy103',
+    author_email='userchouenthusiast@gmail.com',
+    description='Simple cryptography wrapper created with threading.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://github.com/newguy103/pycrypter',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
